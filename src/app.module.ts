20e74e18dd644996da30306dde8f9cbc75700c10
Jennie-geo <isintumejenny@gmail.com>
2022-07-21 15:01:13 +0100

    nestjs setup

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+//this is the main module, that is the app that will import other modules.
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule],
+})
+export class AppModule {}
