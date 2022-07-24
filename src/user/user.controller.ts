dc39c649a49f93c691c2c95bf9006318064d3cc5
Jennie-geo <isintumejenny@gmail.com>
2022-07-24 22:05:17 +0100

    added edit user logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { getUser } from '../auth/decorators';
 import { JwtGuards } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuards)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('loginuser')
   getUser(@getUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@getUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
